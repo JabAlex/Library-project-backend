8ee4d44833f228870ec5e752578c153ef7b34d78
Aleksander Jablonski <jablonski.aleksander98@gmail.com>
2023-05-25 19:59:59 +0200

    first commit

---
+++ src/test/java/com/libraryproject/LibraryProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
